b587e6187c3d4576f99b4a93c586006acce2842d
Rocky Ngangom <neorockyng@gmail.com>
2024-11-22 13:55:14 +0530

    Instamojo config

---
+++ config/instamojo.php
@@ -0,0 +1,7 @@
+<?php
+
+return [
+    'api_key' => env('INSTAMOJO_API_KEY'),
+    'auth_token' => env('INSTAMOJO_AUTH_TOKEN'),
+    'url' => 'https://test.instamojo.com/api/1.1/', // or the live URL
+];
